1ced37009d63c381e71bc3fec9a22051af928c61
jesseysikoi-prog <jesseysikoi@gmail.com>
2026-07-14 12:36:49 +0300

    First Python Activity

---
+++ Lesson 1/Activity 1.py
@@ -0,0 +1,12 @@
+# Printing a greet method
+print("Hello, world!")
+
+# Printing my name
+print("My name is Jessey.")
+
+# Printing a fun fact about myself
+# Printing ("Printing i like playing football.")
+print("I love playing football.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
