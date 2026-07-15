cc73d88e491b9b6678503e5ab35ed5591dad3c8b
jesseysikoi-prog <jesseysikoi@gmail.com>
2026-07-15 20:35:52 +0300

    Getting started with programming

---
+++ Lesson 2/Activity3.py
@@ -0,0 +1,14 @@
+# Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(5)
+
+#Next line after print
+print("Print Python \n")
+
+#Pass multiple arguments of printing
+print("hello",10)
+
+#End argument of print statement
+print("welcome to ", end= "*")
